ecc2167603d074cc03a7602022fdfe164ef8884f
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-05-12 19:48:42 -0700

    Add project files.

---
+++ COMP003B.SP25.FinalProject.PulidoA/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.PulidoA.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public int GenreId { get; set; }
+
+        public int PlatformId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public bool Completed { get; set; }
+
+    }
+}
---
+++ COMP003B.SP25.FinalProject.PulidoA/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.PulidoA.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public List<Game> Games { get; set; }
+
+    }
+}
