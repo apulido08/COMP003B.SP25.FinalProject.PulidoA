cd3b95a6c7c73bf0030aa71615b6153f0ee6fbc4
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-05-12 19:53:09 -0700

    created Data folder

---
+++ COMP003B.SP25.FinalProject.PulidoA/Data/GameDbContext.cs
@@ -0,0 +1,16 @@
+﻿using COMP003B.SP25.FinalProject.PulidoA.Models;
+using Microsoft.EntityFrameworkCore;
+namespace COMP003B.SP25.FinalProject.PulidoA.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options) : base(options) { }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Platform> Platforms { get; set; }
+
+    }
+}
